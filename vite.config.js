ae0b7bde6c86d72b7338368f76b187176c99f3ab
ahmed <you@example.com>
2026-07-29 14:39:53 +0300

    Configure GitHub Pages deployment

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/sla-agenda/", // Ensures relative asset paths work on GitHub Pages
+});
